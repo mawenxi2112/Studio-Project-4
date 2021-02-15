a50172f4e9c8ac683b9d223076f228bd86245047
mawenxi2112 <39835502+mawenxi2112@users.noreply.github.com>
2021-02-16 01:48:09 +0800

    Added player animation (incomplete)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,6 +12,8 @@ public class PlayerMovement : MonoBehaviour
 
     Vector2 movement;
 
+    int direction = 2; // Up = 0 Right = 1 Down = 2 Left = 3
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -21,6 +23,16 @@ public class PlayerMovement : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        if (Mathf.Abs(movement.x) > Mathf.Abs(movement.y))
+        {
+            // Horizontal is stronger, check for left or right
+            if (movement.x < 0)
+                direction = 3;
+            else if (movement.x > 0)
+                direction = 1;
+        }
+
     }
     
     void FixedUpdate()
