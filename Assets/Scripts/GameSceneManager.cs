2618b8b3ba0858cc479f60da0a135f58bae798f0
mawenxi2112 <39835502+mawenxi2112@users.noreply.github.com>
2021-02-15 22:00:39 +0800

    Added base files

---
+++ Assets/Scripts/GameSceneManager.cs
@@ -0,0 +1,266 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AsteroidsGameManager.cs" company="Exit Games GmbH">
+//   Part of: Asteroid demo
+// </copyright>
+// <summary>
+//  Game Manager for the Asteroid Demo
+// </summary>
+// <author>developer@exitgames.com</author>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Collections;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+using Photon.Pun;
+using Photon.Realtime;
+using Photon.Pun.UtilityScripts;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
+
+public class GameSceneManager : MonoBehaviourPunCallbacks
+{
+    public static GameSceneManager Instance = null;
+
+    public Text InfoText;
+
+    //public GameObject[] AsteroidPrefabs;
+
+    #region UNITY
+
+    public void Awake()
+    {
+        Instance = this;
+    }
+
+    public override void OnEnable()
+    {
+        base.OnEnable();
+
+        CountdownTimer.OnCountdownTimerHasExpired += OnCountdownTimerIsExpired;
+    }
+
+    public void Start()
+    {
+        Hashtable props = new Hashtable
+            {
+                {GameData.PLAYER_LOADED_LEVEL, true}
+            };
+        PhotonNetwork.LocalPlayer.SetCustomProperties(props);
+        StartGame();
+    }
+
+    public override void OnDisable()
+    {
+        base.OnDisable();
+
+        CountdownTimer.OnCountdownTimerHasExpired -= OnCountdownTimerIsExpired;
+    }
+
+    #endregion
+
+    #region COROUTINES
+
+/*    private IEnumerator SpawnAsteroid()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(GameDataa.ASTEROIDS_MIN_SPAWN_TIME, AsteroidsGame.ASTEROIDS_MAX_SPAWN_TIME));
+
+            Vector2 direction = Random.insideUnitCircle;
+            Vector3 position = Vector3.zero;
+
+            if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+            {
+                // Make it appear on the left/right side
+                position = new Vector3(Mathf.Sign(direction.x) * Camera.main.orthographicSize * Camera.main.aspect, 0, direction.y * Camera.main.orthographicSize);
+            }
+            else
+            {
+                // Make it appear on the top/bottom
+                position = new Vector3(direction.x * Camera.main.orthographicSize * Camera.main.aspect, 0, Mathf.Sign(direction.y) * Camera.main.orthographicSize);
+            }
+
+            // Offset slightly so we are not out of screen at creation time (as it would destroy the asteroid right away)
+            position -= position.normalized * 0.1f;
+
+
+            Vector3 force = -position.normalized * 1000.0f;
+            Vector3 torque = Random.insideUnitSphere * Random.Range(500.0f, 1500.0f);
+            object[] instantiationData = { force, torque, true };
+
+            PhotonNetwork.InstantiateRoomObject("BigAsteroid", position, Quaternion.Euler(Random.value * 360.0f, Random.value * 360.0f, Random.value * 360.0f), 0, instantiationData);
+        }
+    }
+*/
+    private IEnumerator EndOfGame(string winner, int score)
+    {
+        float timer = 5.0f;
+
+        while (timer > 0.0f)
+        {
+            InfoText.text = string.Format("Player {0} won with {1} points.\n\n\nReturning to login screen in {2} seconds.", winner, score, timer.ToString("n2"));
+
+            yield return new WaitForEndOfFrame();
+
+            timer -= Time.deltaTime;
+        }
+
+        PhotonNetwork.LeaveRoom();
+    }
+
+    #endregion
+
+    #region PUN CALLBACKS
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("DemoAsteroids-LobbyScene");
+    }
+
+    public override void OnLeftRoom()
+    {
+        PhotonNetwork.Disconnect();
+    }
+
+    public override void OnMasterClientSwitched(Player newMasterClient)
+    {
+        if (PhotonNetwork.LocalPlayer.ActorNumber == newMasterClient.ActorNumber)
+        {
+            //StartCoroutine(SpawnAsteroid());
+        }
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        // Force the game to quit/end
+
+        //CheckEndOfGame();
+    }
+
+    public override void OnPlayerPropertiesUpdate(Player targetPlayer, Hashtable changedProps)
+    {
+        if (changedProps.ContainsKey(GameData.PLAYER_LIVES))
+        {
+            CheckEndOfGame();
+            return;
+        }
+
+        if (!PhotonNetwork.IsMasterClient)
+        {
+            return;
+        }
+
+
+        // if there was no countdown yet, the master client (this one) waits until everyone loaded the level and sets a timer start
+        int startTimestamp;
+        bool startTimeIsSet = CountdownTimer.TryGetStartTime(out startTimestamp);
+
+        if (changedProps.ContainsKey(GameData.PLAYER_LOADED_LEVEL))
+        {
+            if (CheckAllPlayerLoadedLevel())
+            {
+                if (!startTimeIsSet)
+                {
+                    CountdownTimer.SetStartTime();
+                }
+            }
+            else
+            {
+                // not all players loaded yet. wait:
+                Debug.Log("setting text waiting for players! ", this.InfoText);
+                InfoText.text = "Waiting for other players...";
+            }
+        }
+
+    }
+
+    #endregion
+
+
+    // called by OnCountdownTimerIsExpired() when the timer ended
+    private void StartGame()
+    {
+        Debug.Log("StartGame!");
+
+        Vector3 position = new Vector3(0.0f, 0.0f, 0.0f);
+        Quaternion rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+
+        GameObject player = PhotonNetwork.Instantiate("Player", position, rotation, 0);      // avoid this call on rejoin (ship was network instantiated before)
+
+        if (player.GetComponent<PhotonView>().IsMine)
+        {
+            // Do targetting of camera here.
+        }
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            
+        }
+    }
+
+    private bool CheckAllPlayerLoadedLevel()
+    {
+        foreach (Player p in PhotonNetwork.PlayerList)
+        {
+            object playerLoadedLevel;
+
+            if (p.CustomProperties.TryGetValue(GameData.PLAYER_LOADED_LEVEL, out playerLoadedLevel))
+            {
+                if ((bool)playerLoadedLevel)
+                {
+                    continue;
+                }
+            }
+
+            return false;
+        }
+
+        return true;
+    }
+
+    private void CheckEndOfGame()
+    {
+        bool allDestroyed = true;
+
+        foreach (Player p in PhotonNetwork.PlayerList)
+        {
+            object lives;
+            if (p.CustomProperties.TryGetValue(GameData.PLAYER_LIVES, out lives))
+            {
+                if ((int)lives > 0)
+                {
+                    allDestroyed = false;
+                    break;
+                }
+            }
+        }
+
+        if (allDestroyed)
+        {
+            if (PhotonNetwork.IsMasterClient)
+            {
+                StopAllCoroutines();
+            }
+
+            string winner = "";
+            int score = -1;
+
+            foreach (Player p in PhotonNetwork.PlayerList)
+            {
+                if (p.GetScore() > score)
+                {
+                    winner = p.NickName;
+                    score = p.GetScore();
+                }
+            }
+
+            StartCoroutine(EndOfGame(winner, score));
+        }
+    }
+
+    private void OnCountdownTimerIsExpired()
+    {
+        StartGame();
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+    
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
