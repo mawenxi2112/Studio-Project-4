fa7f57a62b59295b88abbd876ae2fd853c761169
mawenxi2112 <39835502+mawenxi2112@users.noreply.github.com>
2021-02-16 19:26:10 +0800

    Fixed mouse to world position

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -21,7 +21,7 @@ public class PlayerAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //m_weapon.GetComponent<Transform>().position = ScreenToWorldPos; /*Vector2.ClampMagnitude(new Vector2(ScreenToWorldPos.x, ScreenToWorldPos.y), GetComponent<PlayerData>().m_weaponRadius);*/
+        m_weapon.GetComponent<Transform>().position = new Vector2(transform.position.x, transform.position.y) + Vector2.ClampMagnitude(new Vector2(ScreenToWorldPos.x, ScreenToWorldPos.y), GetComponent<PlayerData>().m_weaponRadius);
 
     }
 
@@ -33,15 +33,14 @@ public class PlayerAttack : MonoBehaviour
     void OnGUI()
     {
         Event currentEvent = Event.current;
-        Vector2 mousePos = new Vector2();
+        Vector3 mousePos = Input.mousePosition;
 
         // Get the mouse position from Event;
         // Note that the y position from Event is inverted;
 
-        mousePos.x = currentEvent.mousePosition.x;
-        mousePos.y = camera.pixelHeight - currentEvent.mousePosition.y;
+        ScreenToWorldPos = camera.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 0));
 
-        ScreenToWorldPos = camera.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, camera.nearClipPlane));
+
 
         GUILayout.BeginArea(new Rect(20, 20, 250, 120));
         GUILayout.Label("Screen pixels: " + camera.pixelWidth + ":" + camera.pixelHeight);
