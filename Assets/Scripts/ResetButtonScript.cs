be03957822891cbcd047719ab1ee69c04aaaa8ac
Eucil <54037664+Eucil@users.noreply.github.com>
2021-02-17 15:50:24 +0800

    Added Reset Button

---
+++ Assets/Scripts/ResetButtonScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetButtonScript : MonoBehaviour
+{
+    public Animator animator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+	void OnTriggerStay2D(Collider2D collision)
+	{
+        if (collision.CompareTag("Player") || collision.CompareTag("Objects"))
+        {
+            if (animator.GetFloat("TriggerChange") == 0)
+            {
+                animator.SetFloat("TriggerChange", 1);
+            }
+        }
+    }
+
+	void OnTriggerExit2D(Collider2D collision)
+	{
+        if (animator.GetFloat("TriggerChange") == 1)
+        {
+            animator.SetFloat("TriggerChange", 0);
+        }
+    }
+}
