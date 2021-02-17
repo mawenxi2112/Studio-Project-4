6386cadf987e9f84e17c010e42400a299aff3410
Eucil <54037664+Eucil@users.noreply.github.com>
2021-02-17 11:58:19 +0800

    Edited bomb functions

--- Assets/Scripts/BombScript.cs
+++ Assets/Scripts/BombScript.cs
@@ -5,22 +5,25 @@ using UnityEngine;
 public class BombScript : MonoBehaviour
 {
     public Animator animator;
-    public BoxCollider2D boxCollider;
     public CircleCollider2D circleCollider;
 
     public bool startCountDown;
+    public bool isBlowingUp;
     
     float Countdown = 0;
     double tickDuration = 0;
     double tickRequiredToChange = 0.2;
 
+    private float regularColliderSize = 0.35f;
+    private float expandedColliderSize = 0.6637768f;
+
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
-        boxCollider = GetComponent<BoxCollider2D>();
         circleCollider = GetComponent<CircleCollider2D>();
         startCountDown = false;
+        isBlowingUp = false;
     }
 
     // Update is called once per frame
@@ -38,26 +41,25 @@ public class BombScript : MonoBehaviour
 
                 if (Countdown >= 10)
                 {
-                    boxCollider.enabled = !boxCollider.enabled;
                     circleCollider.enabled = !circleCollider.enabled;
+                    circleCollider.radius = expandedColliderSize;
+                    isBlowingUp = true;
                 }
             }
 
             if (animator.GetCurrentAnimatorStateInfo(0).IsName("Bomb_Despawn"))
             {
                 gameObject.SetActive(false);
+                Destroy(this);
             }
         }
     }
 
-	void OnCollisionEnter2D(Collision2D collision)
-	{
-        // This function is use to do damaging of other objects around the surroundings
-        collision.gameObject.SetActive(false);
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (isBlowingUp)
+        {
+            collision.gameObject.SetActive(false);
+        }
     }
-
-	void OnTriggerStay2D(Collider2D collider)
-	{
-		// This function use to do picking of bomb
-	}
 }
