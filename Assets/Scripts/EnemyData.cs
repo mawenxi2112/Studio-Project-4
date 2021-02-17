51e3c85b490a850768b4830d08bca5a1eaa3da27
Eucil <54037664+Eucil@users.noreply.github.com>
2021-02-17 23:09:36 +0800

    Added some waypoints

---
+++ Assets/Scripts/EnemyData.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyData : MonoBehaviour
+{
+    public int m_currentHealth;
+    public int m_maxHealth;
+    public float m_currentMoveSpeed;
+    public float m_maxMoveSpeed;
+    public bool m_iFrame;
+    public double m_iFrameCounter;
+    public double m_iFrameThreshold;
+
+    public float m_weaponRadius;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_iFrame)
+        {
+            // Render a different colour during iFrame
+
+            m_iFrameCounter += Time.deltaTime;
+
+            if (m_iFrameCounter >= m_iFrameThreshold)
+            {
+                m_iFrame = false;
+                m_iFrameCounter = 0f;
+            }
+        }
+    }
+    public void SetCurrentHealth(int value)
+    {
+        m_currentHealth = value;
+    }
+
+    public void SetCurrentMoveSpeed(float value)
+    {
+        m_currentMoveSpeed = value;
+    }
+}
--- Assets/Scripts/NavMeshAgentScript.cs
+++ Assets/Scripts/NavMeshAgentScript.cs
@@ -1,18 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NavMeshAgentScript : MonoBehaviour
 {
+    [SerializeField] 
+    public Transform target;
+
+    private NavMeshAgent agent;
+
+    public Vector2 m_dir;
+    public Vector2 m_previousPosition;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+        m_previousPosition = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (target != null)
+            agent.SetDestination(target.position);
     }
+
+	void FixedUpdate()
+	{
+        m_dir = (new Vector2(gameObject.transform.position.x, gameObject.transform.position.y) - m_previousPosition).normalized;
+
+        m_previousPosition = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
+	}
 }
