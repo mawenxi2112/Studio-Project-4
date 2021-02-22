270b9471bf63d1c1a7acdb599dc11d056b31202f
mawenxi2112 <39835502+mawenxi2112@users.noreply.github.com>
2021-02-22 16:16:56 +0800

    Added enemy gameobject sorting Rpc

--- Assets/Scripts/LobbyManager.cs
+++ Assets/Scripts/LobbyManager.cs
@@ -183,7 +183,7 @@ public class LobbyManager : MonoBehaviourPunCallbacks
         {
             PhotonNetwork.CurrentRoom.IsOpen = false;
             PhotonNetwork.CurrentRoom.IsVisible = false;
-            Debug.Log("AllPlayerReady");
+            Debug.Log("Loading Game Scene!!!!!!!!!!!!!");
             PhotonNetwork.LoadLevel("Level1Scene");
         }
 
