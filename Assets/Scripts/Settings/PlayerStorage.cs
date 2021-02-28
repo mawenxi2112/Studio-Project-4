ceecc789c98af0bd84e5360f6880ab8ff3efc8c7
VenkyGG <choy.cheng.feng@gmail.com>
2021-02-28 17:04:33 +0800

    Reduced player starting items.

--- Assets/Scripts/Settings/PlayerStorage.cs
+++ Assets/Scripts/Settings/PlayerStorage.cs
@@ -6,20 +6,20 @@ using UnityEngine;
 
 public static class PlayerStorage
 {
-    public static int maxHealth = 10;
+    public static int maxHealth = 5;
 
     public static int speed = 350;
 
     public static int damage = 1;
 
-    public static int coins = 10000;
+    public static int coins = 0;
 
     public static void Load()
     {
-        maxHealth = PlayerPrefs.GetInt("PMaxHealth", 10);
+        maxHealth = PlayerPrefs.GetInt("PMaxHealth", 5);
         speed = PlayerPrefs.GetInt("PMaxSpeed", 350);
         damage = PlayerPrefs.GetInt("PDamage", 1);
-        coins = PlayerPrefs.GetInt("PCoins", 10000);
+        coins = PlayerPrefs.GetInt("PCoins", 0);
     }
 
     public static void Save()
